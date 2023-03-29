9b634afd8334ae4223d7eb281286e785b24c423f
RA80533 <32469082+RA80533@users.noreply.github.com>
2023-03-29 18:59:15 -0400

    Extract functions from `SpiderFootHelpers`

--- spiderfoot/__init__.py
+++ spiderfoot/__init__.py
@@ -3,6 +3,6 @@ from .event import SpiderFootEvent
 from .threadpool import SpiderFootThreadPool
 from .plugin import SpiderFootPlugin
 from .target import SpiderFootTarget
-from .helpers import SpiderFootHelpers
+from . import helpers as SpiderFootHelpers
 from .correlation import SpiderFootCorrelator
 from spiderfoot.__version__ import __version__
