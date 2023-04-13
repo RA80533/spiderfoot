f481254afa36b3b9dad87f5efcd4be9ab1c2f2e0
RA80533 <32469082+RA80533@users.noreply.github.com>
2023-04-13 15:00:17 +0000

    Extract functions from `SpiderFootHelpers`
    Squashed commit of the following:
    
    commit 64f0c741294a606159dfdd0fb04c98d247b8a295
    Author: RA80533 <32469082+RA80533@users.noreply.github.com>
    Date:   Sat Apr 8 09:38:42 2023 +0000
    
        Remove local import
    
    commit c4ba817f784412c36cf36d032c465aa3918792b0
    Author: RA80533 <32469082+RA80533@users.noreply.github.com>
    Date:   Wed Mar 29 19:08:53 2023 -0400
    
        Reorder imports
    
    commit add844e006f990b366315ec49dac137f18e0037d
    Author: RA80533 <32469082+RA80533@users.noreply.github.com>
    Date:   Wed Mar 29 18:59:15 2023 -0400
    
        Extract functions from `SpiderFootHelpers`

--- spiderfoot/__init__.py
+++ spiderfoot/__init__.py
@@ -3,6 +3,5 @@ from .event import SpiderFootEvent
 from .threadpool import SpiderFootThreadPool
 from .plugin import SpiderFootPlugin
 from .target import SpiderFootTarget
-from .helpers import SpiderFootHelpers
 from .correlation import SpiderFootCorrelator
 from spiderfoot.__version__ import __version__
